8f5f9aee81e93591af78b4c9ad9b330415cce02d
Tom Houlé <tom@tomhoule.com>
2019-11-28 08:31:45 +0100

    Commit final state after meetup

--- script.ts
+++ script.ts
@@ -4,6 +4,62 @@ const photon = new Photon({});
 
 async function main() {
   console.log("hello Karlsruhe!");
+
+  await photon.connect();
+
+  await photon.users.deleteMany({});
+  await photon.dogs.deleteMany({});
+
+  const tom = await photon.users.create({
+    data: {
+      email: "tom@example.com",
+      name: "tom",
+    },
+  });
+
+  const norbert = await photon.dogs.create({
+    include: {
+      likedBy: true,
+    },
+    data: {
+      name: "norbert",
+      likedBy: {
+        connect: {
+          email: tom.email,
+        },
+      },
+    },
+  });
+
+  const oslo = await photon.dogs.create({
+    select: {
+      name: true,
+      likedBy: {
+        select: {
+          email: true,
+        },
+      },
+    },
+    data: {
+      name: "oslo",
+      likedBy: {
+        create: {
+          email: "oslo-human@example.com",
+          name: "oslo’s human",
+        },
+      },
+    },
+  });
+
+  const recap = await photon.dogs.findMany({
+    include: {
+      likedBy: true,
+    },
+  });
+
+  console.log(JSON.stringify(recap, undefined, 2));
+
+  console.log(await photon.users.count(), " user in the database");
 }
 
 main()
